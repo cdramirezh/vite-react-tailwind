cbd424396288a13af3452295592ebfb09d2a6be0
higiniecito <cdramirezh@unal.edu.co>
2023-06-08 14:59:17 -0500

    Card

---
+++ src/components/Navbar/Card/index.jsx
@@ -0,0 +1,25 @@
+const Card = () => {
+	return (
+		<div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
+			<figure className="relative mb-2 w-full h-4/5">
+				<span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
+					Electronics
+				</span>
+				<img
+					className="w-full h-full object-cover rounded-lg"
+					src="https://placekitten.com/300/500"
+					alt="headphones"
+				/>
+				<button className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">
+					+
+				</button>
+			</figure>
+			<p className="flex justify-between">
+				<span className="text-sm font-light">Headphones</span>
+				<span className="text-sm font-light">$300</span>
+			</p>
+		</div>
+	);
+};
+
+export { Card };
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import './home.css'
+import { Card } from '../../components/Navbar/Card'
 
 function Home() {
 	return (
 		<div className='bg-red-200'>
 			Home
+			<Card />
 		</div>
 	)
 }
