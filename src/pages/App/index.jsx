653795c0a69c37404dd03ed6b8fe7758cf26138a
higiniecito <cdramirezh@unal.edu.co>
2023-06-08 11:55:14 -0500

    Link pages with react router

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,24 +1,32 @@
-import { Home } from '../Home'
-import { MyAccount } from '../MyAccount'
-import { MyOrder } from '../MyOrder'
-import { MyOrders } from '../MyOrders'
-import { NotFound } from '../NotFound'
-import { SingIn } from '../SingIn'
-import './App.css'
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import { Home } from "../Home";
+import { MyAccount } from "../MyAccount";
+import { MyOrder } from "../MyOrder";
+import { MyOrders } from "../MyOrders";
+import { NotFound } from "../NotFound";
+import { SingIn } from "../SingIn";
+import "./App.css";
 
+const AppRoutes = () => {
+	let routes = useRoutes([
+		{ path: "/", element: <Home /> },
+		{ path: "/MyAccount", element: <MyAccount /> },
+		{ path: "/MyOrder", element: <MyOrder /> },
+		{ path: "/MyOrders", element: <MyOrders /> },
+		{ path: "/SingIn", element: <SingIn /> },
+		{ path: "/*", element: <NotFound /> },
+	]);
+	return routes;
+};
 
-function App() {
+const App = () => {
 	return (
-		<div className='bg-lime-200'>
-			¡Hola, mundo bebé!
-			<Home />
-			<MyAccount />
-			<MyOrder />
-			<MyOrders />
-			<NotFound />
-			<SingIn />
-		</div>
-	)
-}
+		<>
+			<BrowserRouter>
+				<AppRoutes />
+			</BrowserRouter>
+		</>
+	);
+};
 
-export default App
+export default App;
