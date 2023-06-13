b26ae6ae8058312a7c2ccc4474192a9d1e472b3a
higiniecito <cdramirezh@unal.edu.co>
2023-06-13 11:04:32 -0500

    More folder moving

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Card } from "../../components/Navbar/Card";
+import { Card } from "../../components/Card";
 import { URL } from "../../API";
 import "./home.css";
 
