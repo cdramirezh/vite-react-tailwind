033e83bb7a22f6d9b302de217ce0b8501275d5e7
higiniecito <cdramirezh@unal.edu.co>
2023-06-17 11:59:18 -0500

    Filtro por categorías

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,25 +1,68 @@
 import { useContext, useState } from "react";
+import { useLocation } from "react-router-dom";
 import { Card } from "../../components/Card";
 import { ProductDetail } from "../../components/ProductDetail";
 import { ShoppingCartContext } from "../../Context";
 
 function Home() {
-	const { items } = useContext(ShoppingCartContext)
-	const [searchValue, setSearchValue] = useState('')
-	const filteredItems = !!searchValue.length ? items.filter(item => item.title.toString().toLowerCase().includes(searchValue.toString().toLowerCase())) : items
+
+	const location = useLocation().pathname;
+	const { items } = useContext(ShoppingCartContext);
+
+	let itemsFilteredByCat = [];
+	switch (location) {
+		case "/clothes":
+			itemsFilteredByCat = items.filter(
+				(item) => item.category === "women's clothing"
+			);
+			break;
+		case "/electronics":
+			itemsFilteredByCat = items.filter((item) => item.category === "electronics");
+			break;
+		case "/furniture":
+			itemsFilteredByCat = items.filter((item) => item.category === "jewelery");
+			break;
+		case "/toys":
+			itemsFilteredByCat = items.filter((item) => item.category === "men's clothing");
+			break;
+		case "/others":
+			itemsFilteredByCat = items.filter((item) => item.category === "men's clothing" || item.category === "electronics");
+			break;
+		default:
+			itemsFilteredByCat = items;
+			break;
+	}
+
+	const [searchValue, setSearchValue] = useState("");
+	const filteredItems = !!searchValue.length
+		? itemsFilteredByCat.filter((item) =>
+				item.title
+					.toString()
+					.toLowerCase()
+					.includes(searchValue.toString().toLowerCase())
+		  )
+		: itemsFilteredByCat;
+		
 	return (
 		<>
 			<ProductDetail />
 			<div className="relative flex justify-center w-80 mb-4">
 				<h1 className="font-medium text-xl">Appealing Title</h1>
 			</div>
-			<input className="rounded-lg border border-black w-80 p-4 mb-4 focus:outline-none" type="text" placeholder="Search a product" onChange={(e) => setSearchValue(e.target.value)} />
+			<input
+				className="rounded-lg border border-black w-80 p-4 mb-4 focus:outline-none"
+				type="text"
+				placeholder="Search a product"
+				onChange={(e) => setSearchValue(e.target.value)}
+			/>
 			<div className="grid gap-4 place-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full max-w-screen-lg">
 				{filteredItems.map((item) => (
 					<Card key={item.id} item={item} />
 				))}
 			</div>
-			{!filteredItems.length && <p className="text-center">No tenemos esa vaina :(</p>}
+			{!filteredItems.length && (
+				<p className="text-center">No tenemos esa vaina :(</p>
+			)}
 		</>
 	);
 }
